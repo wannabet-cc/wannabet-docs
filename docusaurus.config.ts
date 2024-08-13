cf8c5d852af0f8ad67c3e605c1df0dc72bd34269
Nick Brodeur <106794061+ncale@users.noreply.github.com>
2024-08-13 15:59:57 -0500

    chore: Update site configuration and assets for wannabet

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,20 +3,20 @@ import type { Config } from "@docusaurus/types";
 import type * as Preset from "@docusaurus/preset-classic";
 
 const config: Config = {
-  title: "My Site",
-  tagline: "Dinosaurs are cool",
-  favicon: "img/favicon.ico",
+  title: "wannabet",
+  tagline: "A simple, fully decentralized, p2p betting protocol",
+  favicon: "img/wannabet.svg",
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
+  url: "https://s.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "Onchain Clarity Co.", // Usually your GitHub org/user name.
+  projectName: "wannabet", // Usually your repo name.
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
@@ -40,17 +40,7 @@ const config: Config = {
           editUrl:
             "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
-        blog: {
-          showReadingTime: true,
-          feedOptions: {
-            type: ["rss", "atom"],
-            xslt: true,
-          },
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
-        },
+        blog: false,
         theme: {
           customCss: "./src/css/custom.css",
         },
@@ -60,21 +50,24 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: "img/docusaurus-social-card.jpg",
+    image: "img/replace.jpg",
     navbar: {
-      title: "My Site",
+      title: "wannabet",
       logo: {
-        alt: "My Site Logo",
-        src: "img/logo.svg",
+        alt: "wannabet logo",
+        src: "img/wannabet.svg",
       },
       items: [
         {
-          type: "docSidebar",
-          sidebarId: "tutorialSidebar",
+          label: "Docs",
+          to: "/docs/overview",
           position: "left",
-          label: "Tutorial",
         },
-        { to: "/blog", label: "Blog", position: "left" },
+        // {
+        //   label: "Blog",
+        //   to: "/blog",
+        //   position: "left",
+        // },
         {
           href: "https://github.com/facebook/docusaurus",
           label: "GitHub",
@@ -89,8 +82,12 @@ const config: Config = {
           title: "Docs",
           items: [
             {
-              label: "Tutorial",
-              to: "/docs/intro",
+              label: "Overview",
+              to: "/docs/overview",
+            },
+            {
+              label: "Getting Started",
+              to: "/docs/getting-started",
             },
           ],
         },
@@ -98,16 +95,12 @@ const config: Config = {
           title: "Community",
           items: [
             {
-              label: "Stack Overflow",
-              href: "https://stackoverflow.com/questions/tagged/docusaurus",
-            },
-            {
-              label: "Discord",
-              href: "https://discordapp.com/invite/docusaurus",
+              label: "Telegram",
+              href: "https://s.com",
             },
             {
               label: "Twitter",
-              href: "https://twitter.com/docusaurus",
+              href: "https://s.com",
             },
           ],
         },
@@ -120,12 +113,12 @@ const config: Config = {
             },
             {
               label: "GitHub",
-              href: "https://github.com/facebook/docusaurus",
+              href: "https://github.com/",
             },
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} wannabet`,
     },
     prism: {
       theme: prismThemes.github,
